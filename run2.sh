5d1a82fa3c84bb30d4e2c97c77ecd74ce34a893e
Sayandeep Sen <palani.k@gmail.com>
2022-11-30 11:32:08 +0000

    Add demo scripts

---
+++ run2.sh
@@ -0,0 +1,1 @@
+python3 src/hookpoint_checker.py -t flow_dissector -f ./asset/bpf_helper_mappings/helper_hookpoint_map.json -s xdp  -l ./examples/suricata-test/xdp_filter.c -m ./examples/suricata-test/compiling
